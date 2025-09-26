7cd17b0bd536271fa4e552261927470193631e6d
Samita Bomasmudram <samiboma@udel.edu>
2025-09-25 23:49:49 -0400

    Completed Task 8

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,22 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+
+    const ChangeType = () => {
+        setType((prevType) =>
+            prevType === "multiple_choice_question" ?
+                "short_answer_question"
+            :   "multiple_choice_question",
+        );
+    };
+
+    return (
+        <div>
+            <Button onClick={ChangeType}>Change Type</Button>
+            {type === "multiple_choice_question" ?
+                "Multiple Choice"
+            :   "Short Answer"}
+        </div>
+    );
 }
--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,40 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "♥️" | "🍀" | "🪔" | "🎃" | "🎄";
+
+const holidaysByYear: Record<Holiday, Holiday> = {
+    "♥️": "🍀",
+    "🍀": "🪔",
+    "🪔": "🎃",
+    "🎃": "🎄",
+    "🎄": "♥️",
+};
+
+const holidaysByAlphabet: Record<Holiday, Holiday> = {
+    "🎄": "🪔",
+    "🪔": "🎃",
+    "🎃": "🍀",
+    "🍀": "♥️",
+    "♥️": "🎄",
+};
+
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("♥️");
+
+    const cycleByYear = () => {
+        setHoliday(holidaysByYear[holiday]);
+    };
+
+    const cycleByAlphabet = () => {
+        setHoliday(holidaysByAlphabet[holiday]);
+    };
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={cycleByAlphabet}> Advance by Alphabet</Button>
+            <Button onClick={cycleByYear}>Advance By Year</Button>
+        </div>
+    );
 }
